99c7cb00a662bb1ac9ee886c9495ddc084864108
Israel Quiroz <israquirozz@MacBook-Pro-de-Israel.local>
2026-01-23 17:26:08 +0100

    Complete Habits CRUD

--- app/api/v1/habits.py
+++ app/api/v1/habits.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import Session
 from typing import List
 
 from app.api.deps import get_db
-from app.schemas.habit import HabitCreate, HabitResponse
+from app.schemas.habit import HabitCreate, HabitResponse, HabitUpdate
 from app.services.habit_service import HabitService
 
 router = APIRouter(
@@ -11,6 +11,19 @@ router = APIRouter(
     tags=["Habits"]
 )
 
+@router.get("/", response_model=List[HabitResponse])
+def get_habits(
+    db: Session = Depends(get_db)
+):
+    return HabitService.get_all_habits(db)
+
+
+@router.get("/{habit_id}", response_model=HabitResponse)
+def get_habit(
+    habit_id: int,
+    db: Session = Depends(get_db)
+):
+    return HabitService.get_habit_by_id(db, habit_id)
 
 @router.post("/", response_model=HabitResponse)
 def create_habit(
@@ -20,8 +33,17 @@ def create_habit(
     return HabitService.create_habit(db, habit_data)
 
 
-@router.get("/", response_model=List[HabitResponse])
-def get_habits(
+@router.put("/{habit_id}", response_model=HabitResponse)
+def update_habit(
+    habit_id: int,
+    habit_data: HabitUpdate,
     db: Session = Depends(get_db)
 ):
-    return HabitService.get_all_habits(db)
+    return HabitService.update_habit_by_id(db, habit_id, habit_data)
+
+@router.delete("/{habit_id}", response_model=HabitResponse)
+def delete_habit(
+    habit_id: int,
+    db: Session = Depends(get_db)
+):
+    return HabitService.delete_habit_by_id(db,habit_id)
